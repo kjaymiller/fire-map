1909478d95bd17134619b76d9746679b1cbb2d16
Jay Miller <kjaymiller@gmail.com>
2022-08-16 09:01:38 -0700

    rename timer query

--- Get-fire-data/__init__.py
+++ Get-fire-data/__init__.py
@@ -4,7 +4,7 @@ from . import process_gis_data
 from . import update_db
 
 
-async def main(mytimer: func.TimerRequest) -> None:
+async def main(reqTimer: func.TimerRequest) -> None:
     database=os.environ.get("COSMOS_DATABASE", None)
     container=os.environ.get("COSMOS_CONTAINER", None)
     await update_db.load_and_write(gis_id="b8f4033069f141729ffb298b7418b653", database=database, container=container)
