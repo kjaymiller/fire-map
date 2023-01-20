ec6a9564d157b4ad6fa735d1407580e188f0543a
Jay Miller <kjaymiller@gmail.com>
2023-01-19 18:59:40 -0800

    get function_app to working

--- function_app.py
+++ function_app.py
@@ -3,8 +3,8 @@ import os
 
 import azure.functions as func
 
-from src.db.update_db import write_to_cosmos
-from src.viirs.get_fire_data import get_fire_data
+# from src.db.update_db import write_to_cosmos
+# from src.viirs.get_fire_data import get_fire_data
 
 app = func.FunctionApp()
 
@@ -16,10 +16,10 @@ app = func.FunctionApp()
     use_monitor=False,
 ) 
 def load_cosmos(mytimer: func.TimerRequest) -> None:
-    """Fetch the """
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
-    write_to_cosmos(
-            data = get_fire_data(),
-            database=os.environ.get('COSMOS_DATABASE'),
-            container=os.environ.get('COSMOS_CONTAINER'),
-    )
+    """Fetch the time"""
+    logging.info('Yo - Python timer trigger function ran at')
+    # write_to_cosmos(
+    #         data = get_fire_data(),
+    #         database=os.environ.get('COSMOS_DATABASE'),
+    #         container=os.environ.get('COSMOS_CONTAINER'),
+    # )
